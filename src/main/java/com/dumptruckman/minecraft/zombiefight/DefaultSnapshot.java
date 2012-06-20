72e1de4694d38d011bbd9b8664bf5e29055a4881
Jeremy Wood <farachan@gmail.com>
2012-06-20 13:25:54 -0400

    Another possible fix for crazy crash.

--- src/main/java/com/dumptruckman/minecraft/zombiefight/DefaultSnapshot.java
+++ src/main/java/com/dumptruckman/minecraft/zombiefight/DefaultSnapshot.java
@@ -8,8 +8,10 @@ import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
+import org.bukkit.entity.Ageable;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
@@ -30,37 +32,46 @@ class DefaultSnapshot implements Snapshot {
     private static class EntitySnapshot {
         private int x, y, z;
         private Class type;
+        private int age = 0;
+
         private EntitySnapshot(Entity entity) {
             Location loc = entity.getLocation();
             x = loc.getBlockX();
             y = loc.getBlockY();
             z = loc.getBlockZ();
             type = entity.getClass();
+            if (entity instanceof Ageable) {
+                age = ((Ageable) entity).getAge();
+            }
         }
         void spawn(World world) {
             if (!Item.class.isAssignableFrom(type)) {
-                world.spawn(new Location(world, x, y, z), type);
+                Entity entity = world.spawn(new Location(world, x, y, z), type);
+                if (entity instanceof Ageable) {
+                    ((Ageable) entity).setAge(age);
+                }
             }
         }
     }
 
     DefaultSnapshot(World world) {
         worldName = world.getName();
-        for (Entity entity : world.getEntities()) {
-            if (!(entity instanceof Player)) {
-                entityIds.add(entity.getUniqueId());
-                entities.add(new EntitySnapshot(entity));
-            }
+        for (Entity e : world.getEntities()) {
+            snapEntity(e);
         }
     }
 
     @Override
     public void snapshotChunk(Chunk chunk) {
-        for (Entity entity : chunk.getEntities()) {
-            if (!(entity instanceof Player)) {
-                if (entityIds.add(entity.getUniqueId())) {
-                    entities.add(new EntitySnapshot(entity));
-                }
+        for (Entity e : chunk.getEntities()) {
+            snapEntity(e);
+        }
+    }
+
+    private void snapEntity(Entity entity) {
+        if (entity instanceof LivingEntity && !(entity instanceof Player)) {
+            if (entityIds.add(entity.getUniqueId())) {
+                entities.add(new EntitySnapshot(entity));
             }
         }
     }
@@ -88,7 +99,7 @@ class DefaultSnapshot implements Snapshot {
             blockData.getValue().update(true);
         }
         for (Entity entity : world.getEntities()) {
-            if (!(entity instanceof Player)) {
+            if (entity instanceof LivingEntity && !(entity instanceof Player)) {
                 entity.remove();
             }
         }
