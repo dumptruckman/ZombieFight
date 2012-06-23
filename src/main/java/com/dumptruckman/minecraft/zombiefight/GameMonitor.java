4a7052d2f2732205b821236830421f669a19e9a0
Jeremy Wood <farachan@gmail.com>
2012-06-23 16:40:29 -0400

    Added missing files.

---
+++ src/main/java/com/dumptruckman/minecraft/zombiefight/GameMonitor.java
@@ -0,0 +1,132 @@
+package com.dumptruckman.minecraft.zombiefight;
+
+import com.dumptruckman.minecraft.zombiefight.api.Game;
+import com.dumptruckman.minecraft.zombiefight.api.ZombieFight;
+import org.bukkit.Chunk;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.event.block.BlockDispenseEvent;
+import org.bukkit.event.block.BlockFadeEvent;
+import org.bukkit.event.block.BlockFromToEvent;
+import org.bukkit.event.block.BlockGrowEvent;
+import org.bukkit.event.block.BlockIgniteEvent;
+import org.bukkit.event.block.BlockPhysicsEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.LeavesDecayEvent;
+import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.inventory.BrewEvent;
+import org.bukkit.event.inventory.FurnaceBurnEvent;
+import org.bukkit.event.inventory.FurnaceSmeltEvent;
+import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.event.world.WorldUnloadEvent;
+
+public class GameMonitor implements Listener {
+
+    private ZombieFight plugin;
+
+    public GameMonitor(ZombieFight plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void chunkLoad(ChunkLoadEvent event) {
+        Chunk chunk = event.getChunk();
+        Game game = plugin.getGameManager().getGame(chunk.getWorld());
+        if (game.isEnabled()) {
+            game.snapshotChunk(chunk);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void unloadWorld(WorldUnloadEvent event) {
+        if (!event.isCancelled()) {
+            plugin.getGameManager().disableWorld(event.getWorld());
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void blockMonitor(BlockBreakEvent event) {
+        handleBlockEvent(event.getBlock(), event);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void blockMonitor(BlockBurnEvent event) {
+        handleBlockEvent(event.getBlock(), event);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void blockMonitor(BlockDispenseEvent event) {
+        handleBlockEvent(event.getBlock(), event);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void blockMonitor(BlockFadeEvent event) {
+        handleBlockEvent(event.getBlock(), event);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void blockMonitor(BlockFromToEvent event) {
+        handleBlockEvent(event.getBlock(), event);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void blockMonitor(BlockGrowEvent event) {
+        handleBlockEvent(event.getBlock(), event);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void blockMonitor(BlockIgniteEvent event) {
+        handleBlockEvent(event.getBlock(), event);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void blockMonitor(BlockPhysicsEvent event) {
+        handleBlockEvent(event.getBlock(), event);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void blockMonitor(BlockPlaceEvent event) {
+        handleBlockEvent(event.getBlock(), event);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void blockMonitor(BrewEvent event) {
+        handleBlockEvent(event.getBlock(), event);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void blockMonitor(FurnaceBurnEvent event) {
+        handleBlockEvent(event.getBlock(), event);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void blockMonitor(FurnaceSmeltEvent event) {
+        handleBlockEvent(event.getBlock(), event);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void blockMonitor(LeavesDecayEvent event) {
+        handleBlockEvent(event.getBlock(), event);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void blockMonitor(SignChangeEvent event) {
+        handleBlockEvent(event.getBlock(), event);
+    }
+
+    private void handleBlockEvent(Block block, Cancellable event) {
+        if (event.isCancelled()) {
+            return;
+        }
+        Game game = plugin.getGameManager().getGame(block.getWorld());
+        if (game.isEnabled()) {
+            game.snapshotBlock(block);
+        }
+    }
+}
---
+++ src/main/java/com/dumptruckman/minecraft/zombiefight/task/HumanFinderTask.java
@@ -0,0 +1,58 @@
+package com.dumptruckman.minecraft.zombiefight.task;
+
+import com.dumptruckman.minecraft.zombiefight.api.Game;
+import com.dumptruckman.minecraft.zombiefight.api.ZFConfig;
+import com.dumptruckman.minecraft.zombiefight.api.ZombieFight;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public class HumanFinderTask extends GameTask {
+
+    private long beaconTick = 0;
+    private long humanFinder = 0;
+
+    public HumanFinderTask(Game game, ZombieFight plugin) {
+        super(game, plugin, 20L, 20L);
+    }
+
+    public void humanFound() {
+        humanFinder = 0;
+    }
+
+    @Override
+    protected void onRun() {
+        long lastHit = humanFinder - getConfig().get(ZFConfig.HUMAN_FINDER_START);
+        if (lastHit == 0) {
+            strike();
+            tick();
+        } else if (lastHit > 0) {
+            if (beaconTick >= getConfig().get(ZFConfig.HUMAN_FINDER_TICK)) {
+                strike();
+                beaconTick = 0;
+            }
+            tick();
+        } else {
+            beaconTick = 0;
+            humanFinder++;
+        }
+    }
+
+    private void tick() {
+        humanFinder++;
+        beaconTick++;
+    }
+
+    private void strike() {
+        for (Player player : getGame().getWorld().getPlayers()) {
+            if (!getGame().isZombie(player)) {
+                Location loc = player.getLocation();
+                loc.getWorld().strikeLightningEffect(loc);
+            }
+        }
+    }
+
+    @Override
+    public boolean shouldEnd() {
+        return !getGame().hasStarted() || getGame().hasEnded();
+    }
+}
